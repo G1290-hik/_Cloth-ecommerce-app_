b48c0a3c111d302285a7d93cb566cebb93d289e0
Gobind <gbindsingh385@gmail.com>
2024-02-21 23:26:44 +0530

    Architecture Change

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,5 @@
-import 'package:cloth_ecommerce_app/login/signup_ui/login_screen.dart';
-import 'package:cloth_ecommerce_app/login/signup_ui/signup_screen.dart';
+import 'package:cloth_ecommerce_app/Screens/login_screen.dart';
+import 'package:cloth_ecommerce_app/Screens/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
